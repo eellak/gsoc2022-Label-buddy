1a88825e14cb0eaf51c1848addb1b7e4761e9939
GiannisProkopiou <giannisprokopiou@hotmail.com>
2022-10-02 11:17:29 +0300

    Create Project page test.

--- label_buddy/tests/test_pages.py
+++ label_buddy/tests/test_pages.py
@@ -1,4 +1,7 @@
 from django.test import TestCase, Client
+from users.models import User
+
+TestUser = User.objects.create(name='TestUser', password='testuserpassword', can_create_projects=True)
 
 class TestIndexPage(TestCase):
     def test_index(self):
@@ -9,3 +12,17 @@ class TestIndexPage(TestCase):
         # test page that does not exist
         request = self.client.get('/does_not_exist')
         self.assertEqual(request.status_code, 404)
+
+
+class TestCreatProjectPage(TestCase):
+    def test_create_project(self):
+
+        c = Client()
+        response = c.post('/projects/create', {'username': 'TestUserName', 'password': 'testuserpassword'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_page(self):
+        # test page that does not exist
+        c = Client()
+        response = c.post('/projects/create/does_not_exist', {'username': 'TestUserName', 'password': 'testuserpassword'})
+        self.assertEqual(response.status_code, 404)
---
+++ label_buddy/tests/test_projects.py
@@ -0,0 +1,9 @@
+# from django.test import TestCase
+# from .models import Project, User
+
+# class ProjectTestCase(TestCase):
+#     def setUp(self):
+
+#         TestUser = User.objects.create(name='TestUser', email='testuser@test.com', password='123', can_create_projects=True, phone_number="6969696969")
+#         Project.objects.create(title="TestProject", description="TestDiscription", instructions="TestInstructions",
+#         labels="TestLabels",  reviewers=TestUser, annotators=TestUser, managers=TestUser)
