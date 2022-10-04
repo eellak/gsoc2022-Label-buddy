e509aa3fcd9711484ad7ac01a4f132f351bac373
GiannisProkopiou <giannisprokopiou@hotmail.com>
2022-10-04 14:54:46 +0300

    Login unit test.

---
+++ label_buddy/tests/test_models.py
@@ -0,0 +1,121 @@
+from django.contrib.auth import authenticate, login
+from projects.models import Project, Label, PredictionModels
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from tasks.models import Task
+from django.contrib.auth.models import User
+import factory
+
+
+# class UserFactory(DjangoModelFactory):
+
+#     username = factory.Sequence('testuser{}'.format)
+#     email = factory.Sequence('testuser{}@company.com'.format)
+
+#     class Meta:
+#         model = get_user_model()
+
+
+class SigninTest(TestCase):
+
+    def setUp(self):
+        self.TestUser = get_user_model().objects.create_user(username='TestUserName', password='TestUserPassword', email='TestUserName@mail.com')
+        self.TestUser.save()
+
+    def tearDown(self):
+        self.TestUser.delete()
+
+    def test_correct(self):
+        user = authenticate(username='TestUserName', password='TestUserPassword')
+        self.assertTrue((user is not None) and user.is_authenticated)
+
+    def test_wrong_username(self):
+        user = authenticate(username='TestUserWrongName', password='TestUserPassword')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+    def test_wrong_pssword(self):
+        user = authenticate(username='TestUserName', password='TestUserWrongPassword')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+
+class PredictionModelTest(TestCase):
+
+    def setUp(self):
+        self.TestPredictionModel = PredictionModels.objects.create(title='TestPredictionModel')
+        self.TestPredictionModel.save()
+
+    def test_update_prediction_model_title(self):
+        self.TestPredictionModel.title = 'new Title'
+        self.TestPredictionModel.save()
+        self.assertEqual(self.TestPredictionModel.title, 'new Title')
+
+    def tearDown(self):
+        self.TestPredictionModel.delete()
+
+
+class LabelTest(TestCase):
+
+    def setUp(self):
+        self.TestLabel = Label.objects.create(name='TestLabel1')
+        self.TestLabel.save()
+
+    def tearDown(self):
+        self.TestLabel.delete()
+
+
+class UserTest(TestCase):
+
+    def setUp(self):
+        self.TestUser = get_user_model().objects.create_user(username='TestUserName', password='TestUserPassword', email='TestUserName@mail.com')
+        self.TestUser.save()
+
+    def test_update_user_username(self):
+        self.TestUser.username = 'new TestUserName'
+        self.TestUser.save()
+        self.assertEqual(self.TestUser.username, 'new TestUserName')
+
+    def tearDown(self):
+        self.TestUser.delete()
+
+
+class ProjectTest(TestCase):
+
+    def setUp(self):
+
+        self.TestPredictionModel = PredictionModels.objects.create(title='TestPredictionModel')
+        self.TestPredictionModel.save()
+
+        self.TestProject = Project(title="TestProject", prediction_model=self.TestPredictionModel)
+        self.TestProject.save()
+    
+    def test_project_title(self):
+        self.TestProject.title = 'new TestProject'
+        self.TestProject.save()
+        self.assertEqual(self.TestProject.title, 'new TestProject')
+
+
+class TaskTest(TestCase):
+
+    def setUp(self):
+        self.TestUser = get_user_model().objects.create_user(username='TestUserName', password='TestUserPassword', email='TestUserName@mail.com')
+        self.TestUser.save()
+
+        self.TestLabel = Label.objects.create(name='TestLabel1')
+        self.TestLabel.save()
+
+
+        self.TestPredictionModel = PredictionModels.objects.create(title='TestPredictionModel')
+        self.TestPredictionModel.save()
+
+
+        self.TestProject = Project(title="TestProject", prediction_model=self.TestPredictionModel)
+        self.TestProject.save()
+
+        self.task = Task(project=self.TestProject)
+        self.task.save()
+
+    def test_read_task(self):
+        self.assertEqual(self.task.project, self.TestProject)
+
+    def tearDown(self):
+        self.TestProject.delete()
--- label_buddy/tests/test_pages.py
+++ label_buddy/tests/test_pages.py
@@ -1,21 +1,5 @@
 from django.test import TestCase, Client
-# from users.models import User
-from projects.models import Project, Label, PredictionModels
-from tasks.models import Task
-import factory
-from factory.django import DjangoModelFactory
-from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
-from django.contrib.auth import authenticate, login
-
-
-# class UserFactory(DjangoModelFactory):
-
-#     username = factory.Sequence('testuser{}'.format)
-#     email = factory.Sequence('testuser{}@company.com'.format)
-
-#     class Meta:
-#         model = get_user_model()
 
 
 class TestIndexPage(TestCase):
@@ -29,114 +13,17 @@ class TestIndexPage(TestCase):
         request = self.client.get('/does_not_exist')
         self.assertEqual(request.status_code, 404)
 
-
-class SigninTest(TestCase):
-
-    def setUp(self):
-        self.TestUser = get_user_model().objects.create_user(username='TestUserName', password='TestUserPassword', email='TestUserName@mail.com')
-        self.TestUser.save()
-
-    def tearDown(self):
-        self.TestUser.delete()
-
-    def test_correct(self):
-        user = authenticate(username='TestUserName', password='TestUserPassword')
-        self.assertTrue((user is not None) and user.is_authenticated)
-
-    def test_wrong_username(self):
-        user = authenticate(username='TestUserWrongName', password='TestUserPassword')
-        self.assertFalse(user is not None and user.is_authenticated)
-
-    def test_wrong_pssword(self):
-        user = authenticate(username='TestUserName', password='TestUserWrongPassword')
-        self.assertFalse(user is not None and user.is_authenticated)
-
-
-class PredictionModelTest(TestCase):
-
-    def setUp(self):
-        self.TestPredictionModel = PredictionModels.objects.create(title='TestPredictionModel')
-        self.TestPredictionModel.save()
-
-    def test_update_prediction_model_title(self):
-        self.TestPredictionModel.title = 'new Title'
-        self.TestPredictionModel.save()
-        self.assertEqual(self.TestPredictionModel.title, 'new Title')
-
-    def tearDown(self):
-        self.TestPredictionModel.delete()
-
-
-class LabelTest(TestCase):
-
-    def setUp(self):
-        self.TestLabel = Label.objects.create(name='TestLabel1')
-        self.TestLabel.save()
-
-    def tearDown(self):
-        self.TestLabel.delete()
-
-
-class UserTest(TestCase):
+class TestLoginPage(TestCase):
 
     def setUp(self):
         self.TestUser = get_user_model().objects.create_user(username='TestUserName', password='TestUserPassword', email='TestUserName@mail.com')
         self.TestUser.save()
 
-    def test_update_user_username(self):
-        self.TestUser.username = 'new TestUserName'
-        self.TestUser.save()
-        self.assertEqual(self.TestUser.username, 'new TestUserName')
+    def test_login(self):
 
-    def tearDown(self):
-        self.TestUser.delete()
-
-
-class ProjectTest(TestCase):
-
-    def setUp(self):
-
-        self.TestPredictionModel = PredictionModels.objects.create(title='TestPredictionModel')
-        self.TestPredictionModel.save()
-
-        self.TestProject = Project(title="TestProject", prediction_model=self.TestPredictionModel)
-        self.TestProject.save()
-    
-    def test_project_title(self):
-        self.TestProject.title = 'new TestProject'
-        self.TestProject.save()
-        self.assertEqual(self.TestProject.title, 'new TestProject')
-
-
-class TaskTest(TestCase):
-
-    def setUp(self):
-        self.TestUser = get_user_model().objects.create_user(username='TestUserName', password='TestUserPassword', email='TestUserName@mail.com')
-        self.TestUser.save()
-
-        self.TestLabel = Label.objects.create(name='TestLabel1')
-        self.TestLabel.save()
-
-
-        self.TestPredictionModel = PredictionModels.objects.create(title='TestPredictionModel')
-        self.TestPredictionModel.save()
-
-
-        self.TestProject = Project(title="TestProject", prediction_model=self.TestPredictionModel)
-        self.TestProject.save()
-
-        self.task = Task(project=self.TestProject)
-        self.task.save()
-
-    def test_read_task(self):
-        self.assertEqual(self.task.project, self.TestProject)
-
-    def tearDown(self):
-        self.TestProject.delete()
-
-# c = Client()
-# logged_in = c.login(username='TestUserName', password='TestUserPassword')
-# self.assertEqual(logged_in, True)
+        c = Client()
+        logged_in = c.login(username='TestUserName', password='TestUserPassword')
+        self.assertEqual(logged_in, True)
 
 
 # class TestCreatProjectPage(TestCase):
